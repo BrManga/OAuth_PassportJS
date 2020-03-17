e7053106fbe114a6372d4960f2eddd023dc61c90
Bora Manga <bora.mng525@gmail.com>
2020-03-17 18:46:27 +0100

    first commit

---
+++ routes/auth-routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const passport = require("passport");
+
+// auth login
+router.get("/login", (req, res) => {
+  res.render("login", { user: req.user });
+});
+
+//auth logout
+router.get("/logout", (req, res) => {
+  res.send("logging out");
+});
+
+//auth google
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile"]
+  })
+);
+router.get("/google/redirect", (req, res) => {
+  res.send("you reached the redirect URI");
+});
+module.exports = router;
